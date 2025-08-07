153a2378c6141b7e11e8cbcad1c599ebcf789cd4
nasim-ferdous <nasimferdouspust@gmail.com>
2025-08-07 17:02:21 +0600

    added task 4

---
+++ task-4.js
@@ -0,0 +1,16 @@
+// Task 4 (Hard)
+// Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+
+// Input:
+
+const statement = "I am a hard working person";
+const output = statement.split(" ").reverse().join(" ");
+// split er vitor white space diye sentence ta k white space er maddhome
+// prottek ta word split korse..diye split kora ongsho k reverse korse diye
+//  reverse kora ongsho k join er vitor white space diye word gula k add korse
+
+console.log(output);
+
+// Output:
+
+// 'person working hard a am I'
